4536d3175b9eb6122cdf75ef2390d0f17e2e22c3
Theo Massard <massar_t@etna-alternance.net>
2018-03-14 15:11:10 +0100

    add log and notification

--- etnotif.py
+++ etnotif.py
@@ -1,16 +1,41 @@
 """Etnotif script"""
 import os
 import time
+import logging
 
 from etnawrapper import EtnaWrapper
+import notify2
 
+
+def get_logger():
+    logger = logging.getLogger(__name__)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+            '%(asctime)s [%(levelname)s] %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    if os.environ.get('DEBUG'):
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+    return logger
+
+
+def get_notification():
+    notify2.init('etnotif')
+    notifier = notify2.Notification('ETNA', message='initialized etnotify')
+    notifier.show()
+    return notifier
+
+
+logger = get_logger()
 
 def get_client():
     login = os.environ.get('ETNA_USER')
     password = os.environ.get('ETNA_PASS')
     etna = EtnaWrapper(login=login, password=password)
     infos = etna.get_infos()
-    print('logged as {} at {}'.format(infos['login'], infos['login_date']))
+    logger.info('logged as {} at {}'.format(infos['login'], infos['login_date']))
     return etna
 
 
@@ -18,11 +43,14 @@ def get_latest_notification(etna):
     notifications = etna.get_notifications()
     return notifications[0]
 
-def monitor_notifications(client):
+
+def monitor_notifications(client, notifier):
     notif = get_latest_notification(client)
-    print('{} : {}'.format(notif['start'], notif['message']))
+    logger.info('{} : {}'.format(notif['start'], notif['message']))
     for notification in new_notifications(notif, client):
-        print(notification)
+        logger.info(notification)
+        notifier.update('ETNA - New notification', message=notification['message'])
+        notifier.show()
 
 
 def new_notifications(old_notif, client):
@@ -30,16 +58,17 @@ def new_notifications(old_notif, client):
         current_notif = get_latest_notification(client)
         if old_notif != current_notif:
             old_notif = current_notif
-            yield notif
+            yield current_notif
         else:
-            print("Same notif")
+            logger.debug('[OLD] %s : %s', current_notif['start'], current_notif['message'])
             time.sleep(5)
 
 
 def main():
     client = get_client()
-    monitor_notifications(client)
-
+    notifier = get_notification()
+    notify = get_notification()
+    monitor_notifications(client, notify)
 
 
 if __name__ == "__main__":

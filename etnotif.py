b0b23f5d04aa8db20276c90021eeeb035a15ae7b
Theo Massard <massar_t@etna-alternance.net>
2018-03-14 12:36:20 +0100

    get latest notification

--- etnotif.py
+++ etnotif.py
@@ -7,15 +7,19 @@ from etnawrapper import EtnaWrapper
 def get_client():
     login = os.environ.get('ETNA_USER')
     password = os.environ.get('ETNA_PASS')
-    print(login)
-    print(password is not None)
     etna = EtnaWrapper(login=login, password=password)
     print(etna.get_infos())
     return etna
 
 
+def get_latest_notification(etna):
+    notifications = etna.get_notifications()
+    return notifications[0]
+
 def main():
-    get_client()
+    client = get_client()
+    notif = get_latest_notification(client)
+    print(notif)
 
 
 if __name__ == "__main__":

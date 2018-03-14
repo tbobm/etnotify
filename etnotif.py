e42cab82f37424114ab5d18c3ed979566a65caf8
Theo Massard <massar_t@etna-alternance.net>
2018-03-14 13:34:21 +0100

    watch notifications and notify new ones

--- etnotif.py
+++ etnotif.py
@@ -9,7 +9,8 @@ def get_client():
     login = os.environ.get('ETNA_USER')
     password = os.environ.get('ETNA_PASS')
     etna = EtnaWrapper(login=login, password=password)
-    print(etna.get_infos())
+    infos = etna.get_infos()
+    print('logged as {} at {}'.format(infos['login'], infos['login_date']))
     return etna
 
 
@@ -17,18 +18,27 @@ def get_latest_notification(etna):
     notifications = etna.get_notifications()
     return notifications[0]
 
-def main():
-    client = get_client()
+def monitor_notifications(client):
     notif = get_latest_notification(client)
-    print(notif)
+    print('{} : {}'.format(notif['start'], notif['message']))
+    for notification in new_notifications(notif, client):
+        print(notification)
+
+
+def new_notifications(old_notif, client):
     while True:
-        new_notif = get_latest_notification(client)
-        if notif != new_notif:
-            print(new_notif)
-            notif = new_notif
+        current_notif = get_latest_notification(client)
+        if old_notif != current_notif:
+            old_notif = current_notif
+            yield notif
         else:
             print("Same notif")
             time.sleep(5)
+
+
+def main():
+    client = get_client()
+    monitor_notifications(client)
 
 
 
